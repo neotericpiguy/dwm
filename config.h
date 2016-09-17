096d723e9fec0f737f65ff9626c26889cc6d6fb4
jannua <jannua@psl.nmsu.edu>
2016-09-17 12:28:48 -0700

    Update: Fixed background colors
    Chaged status colors to match everything

--- config.h
+++ config.h
@@ -7,14 +7,13 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_white[]       = "#eeeeee";
-static const char col_black[]        = "#000000";
+static const char col_white[]       = "#ffffff";
+static const char col_gray[]        = "#aaaaaa";
+static const char col_black[]       = "#000000";
+
 static const char *colors[SchemeLast][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_black },
+	[SchemeNorm] = { col_gray, col_black, col_black },
 	[SchemeSel] =  { col_white, col_black, col_white  },
 };
 
@@ -58,14 +57,13 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_black, "-sf", col_white, NULL };
-static const char *termcmd[]  = { "uxterm","-fg",col_white,"-bg",col_black,"-fn",dmenufont, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray, "-sb", col_black, "-sf", col_white, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	TAGKEYS(                        XK_3,                      2) 
 	{ MODKEY,                       XK_b,      togglebar,      {0} }, 
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } }, 
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
