062b0c87a6a2b9602785ad6d948047437fbf48d3
Jonathan Annua <neotericpiguy@gmail.com>
2019-02-24 16:25:20 -0700

    Fixed: default terminal

--- config.h
+++ config.h
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2]        = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]   = { "xterm", NULL };
+static const char *termcmd[]   = { "st", NULL };
 static const char *slockcmd[]   = { "slock", NULL };
 static const char *chromecmd[] = { ".bin/google-bookmarks",NULL };
 
