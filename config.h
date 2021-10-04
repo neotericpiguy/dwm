ff364dc26378130822743689a57d95415f941784
neotericpiguy <neotericpiguy@gmail.com>
2021-10-03 20:41:48 -0700

    Smaller gap

--- config.h
+++ config.h
@@ -4,19 +4,19 @@
 #include "zoomswap.c"
 
 /* appearance */
-static const unsigned int gappx    = 6;    /* gap pixel between windows */
+static const unsigned int gappx    = 1;    /* gap pixel between windows */
 static const unsigned int borderpx = 1;    /* border pixel of windows */
 static const unsigned int snap     = 32;   /* snap pixel */
 static const int showbar           = 1;    /* 0 means no bar */
 static const int topbar            = 1;    /* 0 means bottom bar */
-static const char* fonts[]         = {"monospace:size=12"};
-static const char dmenufont[] = "monospace:size=10";
+static const char* fonts[]          = {"monospace:size=12"};
+static const char dmenufont[]       = "monospace:size=10";
 static const char normbordercolor[] = "#000000";
-static const char normbgcolor[] = "#000000";
-static const char normfgcolor[] = "#aaaaaa";
-static const char selbordercolor[] = "#ffffff";
-static const char selbgcolor[] = "#000000";
-static const char selfgcolor[] = "#ffffff";
+static const char normbgcolor[]     = "#000000";
+static const char normfgcolor[]     = "#aaaaaa";
+static const char selbordercolor[]  = "#ffffff";
+static const char selbgcolor[]      = "#000000";
+static const char selfgcolor[]      = "#ffffff";
 static const char* colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {normfgcolor, normbgcolor, normbgcolor},
